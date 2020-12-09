85ff1349df4e14aeb6b1f2e013f9df98307ed3b6
ValentinMedvedev <softuna@mail.ru>
2020-12-09 16:18:26 +0400

    added product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 $(function () {
     var mixer = mixitup('.products__inner-box');
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        // ratedFill: "#035bfb",   
+        ratedFill: "#ffa726",
+        readOnly: true,
+        fullStar: true
+        //numStars: 10
+        //maxValue: 2                    
+      });
 })
     
